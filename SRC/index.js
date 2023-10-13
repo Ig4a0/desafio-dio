fe8704a1dcc1145cb72eb04fd880014abea0470d
Ig4a0 <igor.ctrindade@outlook.com>
2023-10-13 16:35:32 -0300

    Desafio DIO completo!

--- SRC/index.js
+++ SRC/index.js
@@ -1,2 +1,18 @@
-let nome= "Gengar"
-console.log ("Meu Pokemon favorito é: " + nome);
+let nomeHeroi = "Mr.Cabelo"
+let quantidadeXP = 6.666
+
+switch(quantidadeXP){
+
+   case 6.666:
+       console.log("O Herói de nome " + nomeHeroi + "está no nivel de Ouro")
+   break
+
+   case 8.025:
+       console.log("O Herói de nome " + nomeHeroi + "está no nivel Ascendente")
+   break
+   
+   case 9.999:
+       console.log("O Herói de nome " + nomeHeroi + "está no nivel Imortal")
+}
+   
+
